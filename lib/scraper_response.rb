ce76aea80c6706cac940f1647b9b8379e3d86e1a
Ali Anwar <ali@alis-mac.local>
2019-07-29 13:19:13 +0500

    ScraperResponse: Updated and Fixed output formats and directory

--- lib/scraper_response.rb
+++ lib/scraper_response.rb
@@ -1,21 +1,21 @@
 class ScraperResponse
   attr_reader :rows, :scrape_file_name, :out_formats
 
-  def initialize(rows:, scrape_file_name:, out_formats: [:json])
+  def initialize(rows, scrape_file_name:, out_formats: [:json])
     @rows = rows
     @scrape_file_name = scrape_file_name
     @out_formats = out_formats
   end
 
   def process
-    generate_or_write_json_file if out_format.include?(:json)
-    generate_or_write_csv_file if out_format.include?(:csv)
+    generate_or_write_json_file if out_formats.include?(:json)
+    generate_or_write_csv_file if out_formats.include?(:csv)
   end
 
   private
 
   def generate_or_write_json_file
-    file = File.open(scrape_file_name, 'a')
+    file = File.open(scrapes_directory("#{scrape_file_name}.json"), 'a')
     file.flock(File::LOCK_EX)
     file.puts rows.collect(&:to_json).join("\n")
     file.close
@@ -23,5 +23,9 @@ class ScraperResponse
 
   def generate_or_write_csv_file
     # Logic
+  end
+
+  def scrapes_directory(scrape_file_name)
+    File.join('scrapes', scrape_file_name)
   end
 end
