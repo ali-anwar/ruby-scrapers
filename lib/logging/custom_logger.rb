3254a2aed7eb8f6010967516b25426282dabe6dc
Ali Anwar <ali@alis-mac.local>
2019-07-29 18:27:05 +0500

    CustomLogger: Updated class method to instance method to pass file name

--- lib/logging/custom_logger.rb
+++ lib/logging/custom_logger.rb
@@ -4,73 +4,65 @@ module Logging
       level: :info
     }
 
+    def initialize(log_file_name)
+      @log_file_name = log_file_name
+    end
+
     LEVELS = %w(debug info warn error fatal unknown).map(&:to_sym)
 
-    class << self
-      def log(*tags, **params)
-        level = determine_log_level(**params)
-        params.delete(:level)
-        message = build_message(*tags, **params)
+    def log(*tags, **params)
+      level = determine_log_level(**params)
+      params.delete(:level)
+      message = build_message(*tags, **params)
 
-        logger.send(level, message)
-      end
+      logger.send(level, message)
+    end
 
-      private
+    private
 
-      def logger
-        return @logger if @logger
+    def logger
+      return @logger if @logger
 
-        @logger = Logger.new File.join('log', "#{env}.log")
-        if production?
-          @logger.level = Logger::ERROR
-        else
-          @logger.level = Logger::INFO
-        end
+      std_out = commandline? ? File.join('log', "#{env}.log") : @log_file_name
+      @logger = Logger.new(std_out)
+      @logger.level = production? ? Logger::ERROR : Logger::INFO
 
-        return @logger
-      end
+      @logger
+    end
 
-      def env
-        ENV['RACK_ENV'] || 'development'
-      end
+    def env
+      ENV['RACK_ENV'] || 'development'
+    end
 
-      def commandline?
-        ENV['SCRAPERS_COMMANDLINE'].present?
-      end
+    def commandline?
+      ENV['SCRAPERS_COMMANDLINE'].present?
+    end
 
-      def production?
-        env == 'production'
-      end
+    def production?
+      env == 'production'
+    end
 
-      def determine_log_level(**params)
-        params.has_key?(:level) && params[:level].to_sym.in?(LEVELS) ? params[:level].to_sym : :info
-      end
+    def determine_log_level(**params)
+      params.has_key?(:level) && params[:level].to_sym.in?(LEVELS) ? params[:level].to_sym : :info
+    end
 
-      def build_message(*tags, **params)
-        tags.map!{ |tag| format_tag(tag) }
-        params = params.map{ |args| format_param(args[0], args[1]) }
+    def build_message(*tags, **params)
+      tags.map!{ |tag| format_tag(tag) }
+      params = params.map{ |args| format_param(args[0], args[1]) }
 
-        tags.join(' ') + ' ' + params.join(' ')
-      end
+      tags.join(' ') + ' ' + params.join(' ')
+    end
 
-      def format_tag(tag)
-        tag = tag.to_s.gsub(/[^\w]/i, '').upcase
-        "[#{tag}]"
-      end
+    def format_tag(tag)
+      tag = tag.to_s.gsub(/[^\w]/i, '').upcase
+      "[#{tag}]"
+    end
 
-      def format_param(key, value)
-        key = key.to_s.gsub(/[^\w]/i, '').downcase
-        value = value.to_s.gsub('"','')
+    def format_param(key, value)
+      key = key.to_s.gsub(/[^\w]/i, '').downcase
+      value = value.to_s.gsub('"','')
 
-        "#{key}=\"#{value}\""
-      end
+      "#{key}=\"#{value}\""
     end
   end
 end
-
-def log(*tags, **params)
-  Logging::CustomLogger.log(*tags, **params)
-end
-
-# Usage
-# log('MAIN_TAG', 'SUB_TAG', key1: value1, key2: value2, error_message: e.message)
